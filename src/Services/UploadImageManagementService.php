966d9df00ad13aae5e1a46025cf1e95920444db9
Manh NV <manhnv4997@gmail.com>
2023-11-29 15:18:33 +0700

    Feature: Add base structure of package

---
+++ src/Services/UploadImageManagementService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Megaads\UploadImageManagement\Services;
+
+
+
+class ManagementService {
+
+    public function __construct() {
+    }
+
+    public function imageCompression() {
+
+    }
+
+    public function createLogUpload() {
+        // create file log: logs/{year}/{month}/log-{day}.txt
+    }
+
+    public function updateRecordInLogFile() {
+
+    }
+
+    public function getPathsUse() {
+
+    }
+
+    public function getPathsNotUse() {
+        
+    }
+    
+
+}
