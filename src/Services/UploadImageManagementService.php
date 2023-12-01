16e2a1dfcf8c4107c9dc291559b4cd6a72fe7e04
Manh NV <manhnv4997@gmail.com>
2023-12-02 01:45:34 +0700

    Optimize: add logic

--- src/Services/UploadImageManagementService.php
+++ src/Services/UploadImageManagementService.php
@@ -2,19 +2,34 @@
 
 namespace Megaads\UploadImageManagement\Services;
 
-
+use Illuminate\Http\UploadedFile;
+use Imagick;
 
 class ManagementService {
 
     public function __construct() {
     }
 
-    public function imageCompression() {
+    public function compressImage($source, $destination, $quality) {
+        $info = getimagesize($source);
 
+        if ($info['mime'] == 'image/jpeg') {
+            $image = imagecreatefromjpeg($source);
+        } elseif ($info['mime'] == 'image/png') {
+            $image = imagecreatefrompng($source);
+        }
+
+        imagejpeg($image, $destination, $quality);
+
+        // Giải phóng bộ nhớ
+        imagedestroy($image);
+
+        return $destination;
     }
 
-    public function createLogUpload() {
+    public function createLogUpload(UploadedFile $fileUpload) {
         // create file log: logs/{year}/{month}/log-{day}.txt
+        dd(123, $fileUpload);
     }
 
     public function updateRecordInLogFile() {
