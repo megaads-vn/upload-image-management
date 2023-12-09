3c121ac8f3952a403a8ac44a469d8152d7dcaa74
Manh NV <manhnv4997@gmail.com>
2023-12-09 11:34:43 +0700

    Optimize: add function get detail logs

--- configs/config.php
+++ configs/config.php
@@ -1,4 +1,7 @@
 <?php
 return [
-    'compression_ratio' => 60 // Compression quality (0 - 100)
+    'compression_ratio' => 60, // Compression quality (0 - 100)
+    'max_height' => 2000, // px
+    'max_width' => 2000, // px
+    'max_size' => 10400,
 ];
